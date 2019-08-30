d005cd340ebf4f1ff4535aacf76efb2da1a69d8b
branmkam <54331071+branmkam@users.noreply.github.com>
2019-08-30 17:13:56 -0400

    Update quickMaths.py

--- quickMaths.py
+++ quickMaths.py
@@ -1,5 +1,9 @@
 def addNums(num1, num2):
-  return num1 + num2
+  sum = num1 + num2
+  if num2 < 0:
+    print(num1 + " - " + (num2/-1) + " = " + sum)
+  else:
+    print(num1 + " + " + num2 + " = " + sum)
 
 addNums(2, 2)
 addNums(4,-1)
